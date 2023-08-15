336e97d99d42b53381eb43d9a7b02ad34d8247c7
richard matheus <richard.tech99@gmail.com>
2023-08-14 21:22:56 -0300

    🚧 fix: deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-const { REST, routes} = require('discord.js');
+const { REST, Routes} = require('discord.js');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -11,3 +11,24 @@ const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
 const commands = [];
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON())
+}
+
+const rest = new REST({version: "10"}).setToken(TOKEN);
+
+(async () => {
+    try {
+        console.log(`Resetando ${commands.length} comandos...`);
+
+        const data = await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            {body: commands}
+        )
+        console.log("Commandos registrados com sucesso!")
+    } catch (error) {
+        console.error(error);
+    }
+})()
